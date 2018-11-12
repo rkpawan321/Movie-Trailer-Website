a96fcf65eb6a6d2f46909c2d0c2fd40f16caa812
rk_intrepid <34833094+rkpawan321@users.noreply.github.com>
2018-11-12 10:43:05 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,26 +1,30 @@
 import webbrowser
 
 class Movie():
-	
-"""A constructor is defined. This constructor is called whenever a new instace of the class is created.
-Attributes:
+    #VALID_RATINGS = ["G" , "PG" , "PG-13" , "R " ]
+
+    
+    """
+      A constructor is defined.
+	  This constructor is called whenever a new instace of the class is created.
+	  Attributes:
         title: Title of the movie
         storyline: Story Line of the movie
         poster_image_url: Pointer to the movie poster.
-        trailer_youtube_url: Pointer to the official movie trailer on YouTube."""
-   
-def __init__(self, movie_title , movie_storyline, poster_image, trailer_youtube):
-    self.title = movie_title
-    self.storyline = movie_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
+        trailer_youtube_url: Pointer to the official movie trailer on YouTube.
+	"""
+    def __init__(self, movie_title , movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-def show_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
     """Plays the embedded YouTube video."""
 
-def show_poster(self):
-    webbrowser.open(self.poster_image_url)
+    def show_poster(self):
+        webbrowser.open(self.poster_image_url)
     """ Shows the embedded poster image. """
                  
         
